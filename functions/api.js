a9011c2dc9ae0be3672148ee9469d74069689acb
srivastavapr <Prateek.Srivastava@tabcorp.com.au>
2024-05-16 15:44:53 +0530

    chnge

--- functions/api.js
+++ functions/api.js
@@ -2,27 +2,14 @@ const express = require('express');
 const serverless = require('serverless-http');
 const app = express();
 const router = express.Router();
+const { Server } = require("socket.io");
+
 
 let records = [];
 
 //Get all students
 router.get('/', (req, res) => {
   res.send('App is running..');
-});
-
-//Create new record
-router.post('/add', (req, res) => {
-  res.send('New record added.');
-});
-
-//delete existing record
-router.delete('/', (req, res) => {
-  res.send('Deleted existing record');
-});
-
-//updating existing record
-router.put('/', (req, res) => {
-  res.send('Updating existing record');
 });
 
 //showing demo records
@@ -44,6 +31,54 @@ router.get('/demo', (req, res) => {
       email: 'lily@gmail.com',
     },
   ]);
+});
+
+const io = new Server(8000, {
+  cors: true,
+});
+
+const emailToSocketIdMap = new Map();
+const socketIdToEmailMap = new Map();
+
+io.on("connection", (socket) => {
+  console.log("Socket connected", socket.id);
+  socket.on("room:join", (data) => {
+    console.log("room:join", data);
+    const { email, room } = data;
+    emailToSocketIdMap.set(email, room);
+    socketIdToEmailMap.set(room, email);
+
+    io.to(room).emit("user:joined", {
+      email,
+      id: socket.id,
+    });
+
+    socket.join(room);
+
+    io.to(socket.id).emit("room:join", { email, room });
+  });
+
+  socket.on("get:userCount", (room) => {
+    const userCount = io.sockets.adapter.rooms.get(room);
+    console.log("userCount", userCount?.size);
+    io.to(room).emit("user:count", { userCount: userCount?.size });
+  });
+
+  socket.on("user:call", ({ to, offer }) => {
+    io.to(to).emit("incoming:call", { from: socket.id, offer });
+  });
+
+  socket.on("call:accepted", ({ to, ans }) => {
+    io.to(to).emit("call:accepted", { from: socket.id, ans });
+  });
+
+  socket.on("peer:nego:needed", ({ to, offer }) => {
+    io.to(to).emit("peer:nego:needed", { from: socket.id, offer });
+  });
+
+  socket.on("peer:nego:done", ({ to, ans }) => {
+    io.to(to).emit("peer:nego:final", { from: socket.id, ans });
+  });
 });
 
 app.use('/.netlify/functions/api', router);
